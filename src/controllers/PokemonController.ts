6d1db72e32cd378a7c34e47588c912bb7a34ad06
Luis Durante <luisdurante2@outlook.com>
2020-09-07 21:36:20 -0300

    Add PokemonController class

--- src/controllers/PokemonController.ts
+++ src/controllers/PokemonController.ts
@@ -1,14 +1,18 @@
 import Pokemon from '../models/Pokemon'
 import { Request, Response } from 'express'
 
-export async function get (req:Request, res:Response):Promise<Response> {
-  const { pokedexId } = req.body
+class PokemonController {
+  async get (req:Request, res:Response):Promise<Response> {
+    const { pokedexId } = req.body
 
-  const pokemon = await Pokemon.findOne({ pokedexNumber: pokedexId })
+    const pokemon = await Pokemon.findOne({ pokedexNumber: pokedexId })
 
-  if (!pokemon) {
-    return res.status(400).json({ error: 'Pokemon not found' })
+    if (!pokemon) {
+      return res.status(400).json({ error: 'Pokemon not found' })
+    }
+
+    return res.status(200).json(pokemon)
   }
-
-  return res.status(200).json(pokemon)
 }
+
+export default new PokemonController()
